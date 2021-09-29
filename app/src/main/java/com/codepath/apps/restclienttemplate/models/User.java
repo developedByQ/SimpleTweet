bd90d7feb069ecdecf348d42d954f01ce6e1dd20
developedByQ <qtaskew@aggies.ncat.edu>
2021-09-29 13:15:38 -0400

    Added the requirements for Part 2

--- app/src/main/java/com/codepath/apps/restclienttemplate/models/User.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/User.java
@@ -3,15 +3,19 @@ package com.codepath.apps.restclienttemplate.models;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
+
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Parcel
 public class User {
 
     public String name;
     public String screenName;
     public String profileImageUrl;
+
 
     public static User fromJson(JSONObject jsonObject) throws JSONException {
         User user = new User();
